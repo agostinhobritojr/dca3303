786f97ed278a91ca6fcc27c56fd1a5bb00f3c100
Agostinho <agostinhobritojr@gmail.com>
2024-10-02 08:34:43 -0300

    .

--- ponteiros/alocacao.c
+++ ponteiros/alocacao.c
@@ -3,15 +3,33 @@
 
 int main(void) {
   int n, i, y[10];
-  int *x;
+  int *x, *z;
   n = 10;  // numero de valores
   x = (int *)malloc(n * sizeof(int));
+  z = (int *)malloc(n * sizeof(int));
+  if (x == NULL) {
+    exit(0);
+  }
+  if (z == NULL) {
+    exit(0);
+  }
   x[0] = 3;
   x[3] = -4;
   for (i = 0; i < n; i++) {
-    printf("x[%d] = %d\n", i, x[i]);
+    z[i] = x[i];
+  }
+  for (i = 0; i < n; i++) {
+    printf("z[%d] = %d\n", i, z[i]);
+  }
+  x[1] = 8;
+  printf("\n");
+  for (i = 0; i < n; i++) {
+    printf("z[%d] = %d\n", i, z[i]);
   }
   free(x);
+  free(z);
+  z = NULL;
+  free(z);
   printf("&y = %p\n", &y);
   printf("&i = %p\n", &i);
   printf("&n = %p\n", &n);
