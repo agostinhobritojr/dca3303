ef22230c3c52d3620b6332c1fef5cdbba275b5cd
Agostinho <agostinhobritojr@gmail.com>
2025-03-21 08:34:11 -0300

    .

--- revisaoc/hello.c
+++ revisaoc/hello.c
@@ -1,6 +1,25 @@
 #include <stdio.h>  // diretiva de inclusao
 
 int main(void) {
-  printf("Alo, mundo!\n");
+  unsigned char x;
+  float y;
+  long int i, max = 1000000;
+
+  x = max;
+
+  printf("max = %ld e x = %d\n", max, x);
+
+  y = 0.1;
+
+  for (i = 0; i < max; i++) {
+    y += 0.1;
+  }
+
+  printf("y = %f\n", y);
+
+  //  x = 457257;
+  // 1101111101000101001
+
+  //  printf("x = %d\n", x);
   return 0;
 }
