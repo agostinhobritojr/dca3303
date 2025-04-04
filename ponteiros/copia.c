122631d3c42bee360d90122f85c91927814f6fbe
Agostinho <agostinhobritojr@gmail.com>
2025-04-04 07:44:02 -0300

    .

--- ponteiros/copia.c
+++ ponteiros/copia.c
@@ -5,17 +5,20 @@ int main(void) {
   int n, *x, *y;
   n = 10;
   x = (int*)malloc(n * sizeof(int));
+  x = (int*)malloc(n * sizeof(int));
+
   for (int i = 0; i < n; i++) {
     x[i] = rand() % 100;
   }
   for (int i = 0; i < n; i++) {
     printf("%2d, ", x[i]);
   }
-  y = x;
   printf("\n");
+  y = x;
   for (int i = 0; i < n; i++) {
     printf("%2d, ", x[i]);
   }
   printf("\n");
   free(x);
+  //  free(y);
 }
