798d19c5c6de87f80e2e51181d07dbccf388f976
Agostinho <agostinhobritojr@gmail.com>
2025-04-04 07:09:49 -0300

    .

---
+++ ponteiros/copia.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int n, *x, *y;
+  n = 10;
+  x = (int*)malloc(n * sizeof(int));
+  for (int i = 0; i < n; i++) {
+    x[i] = rand() % 100;
+  }
+  for (int i = 0; i < n; i++) {
+    printf("%2d, ", x[i]);
+  }
+  y = x;
+  printf("\n");
+  for (int i = 0; i < n; i++) {
+    printf("%2d, ", x[i]);
+  }
+  printf("\n");
+  free(x);
+}
