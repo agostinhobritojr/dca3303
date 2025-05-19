7cb7bda179297abb3e1e99b3732365751e66162b
Agostinho <agostinhobritojr@gmail.com>
2025-05-19 07:44:28 -0300

    .

--- streams/maisum.c
+++ streams/maisum.c
@@ -1,8 +1,11 @@
+#include <iomanip>
 #include <iostream>
 
 int main(void) {
-  int x;
+  float x;
   std::cin >> x;
+  std::cout << std::fixed;
+  std::cout << std::setprecision(2);
   std::cout << x + 1 << std::endl;
   return 0;
 }
