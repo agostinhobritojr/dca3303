c2cbedab61d658c283fe2ad466f871df2afbef79
Agostinho <agostinhobritojr@gmail.com>
2026-03-20 08:28:55 -0300

    .

--- malloc/aloca.c
+++ malloc/aloca.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-  int* x;
+  int *x, *y;
   int n = 10;
-  x = (int*)malloc(10 * sizeof(int));
+  x = (int*)malloc(n * sizeof(int));
+  y = (int*)malloc(n * sizeof(int));
   if (x == NULL) {
     printf("alocacao falhou\n");
     exit(0);
@@ -12,17 +14,23 @@ int main(void) {
   printf("&x = %p\n", &x);
   printf(" x = %p\n", x);
   x[0] = -16;
+  
+  for (int i = 0; i < n; i++) {
+    y[i] = x[i];
+  }
+
+  memcpy(y, x, n * sizeof(int));
+
+  for (int i = 0; i < 10; i++) {
+    printf("%d, ", y[i]);
+  }
+  y[1] = 4;
+  printf("\n");
   for (int i = 0; i < 10; i++) {
     printf("%d, ", x[i]);
   }
   printf("\n");
   free(x);
-  x = NULL;
-
-  for (int i = 0; i < 10; i++) {
-    printf("%d, ", x[i]);
-  }
-
-  printf("(pos-free) x = %p\n", x);
-  free(x);
+  free(y);
+  // x = NULL;
 }
