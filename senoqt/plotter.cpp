197864918f7e39d6e15fd06de7d1777f8a8578a7
agostinhobritojr <agostinhobritojr@gmail.com>
2026-06-03 08:26:57 -0300

    eventos

--- senoqt/plotter.cpp
+++ senoqt/plotter.cpp
@@ -3,11 +3,18 @@
 #include <QBrush>
 #include <QPen>
 #include <cmath>
+#include <QMouseEvent>
+#include <QDebug>
 
 Plotter::Plotter(QWidget *parent)
     : QWidget(parent)
 {
-
+    amp = 1;
+    freq = 1;
+    fase = 0;
+    vel = 0;
+    startTimer(10);
+    setMouseTracking(true);
 }
 
 void Plotter::paintEvent(QPaintEvent *event)
@@ -47,13 +54,50 @@ void Plotter::paintEvent(QPaintEvent *event)
 
     for(int i=1; i<width(); i++){
         x1 = i;
-        y1 = height()/2 - height()/2*std::sin(2 * M_PI * (float)x1/width());
+        y1 = height()/2 - amp*height()/2*std::sin(2 * M_PI *
+                      freq * (float)x1/width() + fase);
         p.drawLine(x0,y0,x1,y1);
         x0 = x1;
         y0 = y1;
     }
 }
 
+void Plotter::timerEvent(QTimerEvent *event)
+{
+    fase = fase + 0.02 * vel;
+    repaint();
+}
+
+void Plotter::mousePressEvent(QMouseEvent *event)
+{
+    emit mudaX(event->pos().x());
+    emit mudaY(event->pos().y());
+    qDebug() << event->pos().x() << event->pos().y();
+}
+
+void Plotter::mouseMoveEvent(QMouseEvent *event)
+{
+    emit mudaX(event->pos().x());
+    emit mudaY(event->pos().y());
+}
+
+
+void Plotter::mudaVelocidade(int vel)
+{
+    this->vel = vel;
+    repaint();
+}
+
+void Plotter::mudaAmplitude(int amp){
+    this->amp = amp/100.0;
+    repaint();
+}
+
+void Plotter::mudaFrequencia(int freq)
+{
+    this->freq = freq;
+    repaint();
+}
 
 
 
--- senoqt/plotter.h
+++ senoqt/plotter.h
@@ -6,11 +6,27 @@
 class Plotter : public QWidget
 {
     Q_OBJECT
+private:
+    float amp;
+    float freq;
+    float fase;
+    float vel;
 public:
     explicit Plotter(QWidget *parent = nullptr);
 
     void paintEvent(QPaintEvent *event);
+    void timerEvent(QTimerEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
 signals:
+    void mudaX(int);
+    void mudaY(int);
+
+public slots:
+    void mudaAmplitude(int amp);
+    void mudaFrequencia(int freq);
+    void mudaVelocidade(int vel);
 };
 
 #endif // PLOTTER_H
