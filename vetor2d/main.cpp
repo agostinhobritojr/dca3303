fa6b04ea29b678280f5de25e07f04a9e6aeda0a1
Agostinho <agostinhobritojr@gmail.com>
2026-09-16 07:25:12 -0300

    .

--- vetor2d/main.cpp
+++ vetor2d/main.cpp
@@ -8,19 +8,38 @@ public:
   void setX(float x_){
     x = x_;
   }
+  void setY(float y_){
+    y = y_;
+  }
 
   float getX(){
     return x;
   }
+
+  void print(void){
+    std::cout << "(" << x << "," << y << ")";
+  }
 };
 
 int main(void){
-  Vetor2d v; // v eh um objeto da classe Vetor2d
- // v.x = 4;
-  v.setX(-3);
-  std::cout << v.getX();
-  std::cout << "tamanho de v = " << sizeof(v) << std::endl;
+  // v1 eh um objeto da classe Vetor2d
+  Vetor2d v1, v2, v3, v4;
+  v1.setX(3);
+  v1.setY(4);
+  std::cout << "norma  = " << v1.norma() << std::endl;
+  std::cout << "angulo = " << v1.teta() << std::endl;
+  std::cout << "v1 = ";
+  v1.print();
+  std::cout << "\n" << std::flush;
 }
+
+
+
+
+
+
+
+
 
 
 
