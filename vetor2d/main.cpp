16bcdd9de80e636daeaf8e66bdc19e6a3c9459cd
Agostinho <agostinhobritojr@gmail.com>
2026-09-16 08:28:28 -0300

    .

--- vetor2d/main.cpp
+++ vetor2d/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 class Vetor2d{
 private:
@@ -19,6 +20,30 @@ public:
   void print(void){
     std::cout << "(" << x << "," << y << ")";
   }
+
+  float norma(void){
+    return std::sqrt(x*x + y*y);
+  }
+
+  float angulo(void){
+    return std::atan2(y, x)*180/M_PI;
+  }
+
+//  v3 = v1.soma(v2);
+  Vetor2d soma(Vetor2d v){
+    Vetor2d ret;
+    ret.x = x + v.x;
+    ret.y = y + v.y;
+    return ret;
+  }
+
+  Vetor2d escalar(float a){
+    Vetor2d ret;
+    ret.x = a * x;
+    ret.y = a * y;
+    return ret;
+  }
+
 };
 
 int main(void){
@@ -26,10 +51,18 @@ int main(void){
   Vetor2d v1, v2, v3, v4;
   v1.setX(3);
   v1.setY(4);
+  v2.setX(-1);
+  v2.setY(5);
   std::cout << "norma  = " << v1.norma() << std::endl;
-  std::cout << "angulo = " << v1.teta() << std::endl;
-  std::cout << "v1 = ";
-  v1.print();
+  std::cout << "angulo = " << v1.angulo() << std::endl;
+
+  v3 = v1.soma(v2);
+
+  std::cout << "v3 = ";
+  v3.print();
+
+  v4 = v1.escalar(3);
+
   std::cout << "\n" << std::flush;
 }
 
